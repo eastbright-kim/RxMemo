5c4155d80b4a9c92f7a4131202ce1c67946bc30f
김동환 <1033513@naver.com>
2021-07-31 17:13:45 +0900

    implement MemoViewModel

---
+++ RxMemo/ViewModel/CommonViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  CommonViewModel.swift
+//  RxMemo
+//
+//  Created by 김동환 on 2021/07/31.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class CommonViewModel: NSObject {
+    let title: Driver<String>
+    let sceneCoordinator: SceneCoordinatorType
+    let storage: MemoStorageType
+    
+    init(title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+        self.title = Observable.just(title).asDriver(onErrorJustReturn: "")
+        self.sceneCoordinator = sceneCoordinator
+        self.storage = storage
+    }
+    
+}
--- RxMemo/ViewModel/MemoListViewModel.swift
+++ RxMemo/ViewModel/MemoListViewModel.swift
@@ -6,7 +6,17 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
-class MemoListViewModel {
+class MemoListViewModel: CommonViewModel {
+    
+    var memoList: Observable<[Memo]> {
+        return storage.memoList()
+    }
+    
+    //의존성 주입 생성자, 바인딩에 사용되는 속성과 메소드 추가. 화면전환 메모 저장 처리. scene coordinator와 메모 스토리지 활용. 뷰모델을 생성하는 시점에 생성자를 통해서 의존성을 주입해야 함.
+    
+    
     
 }
